74bae80b9b8b9bd67400d4c95dcd6d2fbfe73a61
Xie Jiantao <xiejiantao@megvii.com>
2017-11-29 09:59:18 +0800

    fix：添加依赖

--- MGLicenseManagerSDK/LicenseManagerSDK/src/main/java/com/megvii/licensemanager/sdk/jni/NativeLicenseAPI.java
+++ MGLicenseManagerSDK/LicenseManagerSDK/src/main/java/com/megvii/licensemanager/sdk/jni/NativeLicenseAPI.java
@@ -17,6 +17,7 @@ public class NativeLicenseAPI {
      * 这里加载的名称要根据 so 不同的版本号进行修改
      */
     static {
+        System.loadLibrary("MegviiInnerUtils-0.0.2");
         System.loadLibrary("MegviiLicenseManager-0.3.0");
         System.loadLibrary("MegviiLicenseManager-jni-0.3.0");
     }
